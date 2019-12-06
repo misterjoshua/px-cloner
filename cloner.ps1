b302c281e8f73c993aa8871255dbec7cb5efc348
Josh Kellendonk <josh@colloco.com>
2019-12-06 11:31:34 -0700

    Add a powershell port of the script

---
+++ cloner.ps1
@@ -0,0 +1,55 @@
+param(
+    # Source namespace
+    [Parameter(Mandatory=$true, Position=0)]
+    [String]
+    $SourceNamespace,
+
+    # Destination namespace
+    [Parameter(Mandatory=$true, Position=1)]
+    [String]
+    $DestinationNamespace
+)
+
+function Test-Commands() {
+    kubectl version >$null 2>$null
+}
+
+function New-ApplicationClone {
+    $nowEpochFormat = (Get-Date -UFormat "%s") -Split "\." | Select-Object -First 1
+    $cloneName = "clone-$SourceNamespace-to-$DestinationNamespace-$nowEpochFormat"
+
+    $config = @"
+apiVersion: stork.libopenstorage.org/v1alpha1
+kind: ApplicationClone
+metadata:
+    name: $cloneName
+    namespace: kube-system
+spec:
+    sourceNamespace: $SourceNamespace
+    destinationNamespace: $DestinationNamespace
+    replacePolicy: Delete
+"@
+
+    Write-Host "Submitting ApplicationClone request."
+    $config | kubectl -n kube-system apply -f-
+
+    $delay = 1
+    $stage = ""
+    while ($stage -notlike "Final") {
+        Start-Sleep -Seconds $delay
+        $stage = kubectl -n kube-system get applicationclone $cloneName -o jsonpath="{ .status.stage }"
+        Write-Host "Application clone stage: $stage"
+    }
+
+    Write-Host "Restarting pods"
+    kubectl -n $DestinationNamespace delete pods --all
+}
+
+#######
+# Script begins
+########
+
+$ErrorActionPreference = "Stop"
+
+Test-Commands
+New-ApplicationClone
